f381847f5f6f95f463d4604baec95b7fd993823f
Neeraj <neeraj.sidhaye@capgemini.com>
2018-09-26 20:28:41 +0100

    initial commit

---
+++ src/main/java/com/xp/springboot/rest/SpringbootResInMemoryDB/SpringbootResInMemoryDbApplication.java
@@ -0,0 +1,12 @@
+package com.xp.springboot.rest.SpringbootResInMemoryDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootResInMemoryDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootResInMemoryDbApplication.class, args);
+	}
+}
